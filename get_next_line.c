ddff5a5c64caa08ad12394d9740aa635bb262c22
Mario <mario@MacBook-Pro-de-Maeva.local>
2024-09-24 19:04:13 +0200

    Con bonus

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: made-ped <made-ped@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 00:26:56 by made-ped          #+#    #+#             */
-/*   Updated: 2024/03/14 12:27:30 by made-ped         ###   ########.fr       */
+/*   Updated: 2024/09/24 19:00:24 by made-ped         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ static char	*read_line(char *str)
 	if (!str || *str == '\0')
 		return (NULL);
 	i = find_next_line_break(str, i);
-	line = (char *)malloc(sizeof(char) * i + 1);
+	line = (char *)malloc(sizeof(char) * (i + 1));
 	if (!line)
 		return (NULL);
 	i = 0;
@@ -86,62 +86,84 @@ char	*get_next_line(int fd)
 {
 	char		*read_content;
 	int			read_bytes;
-	static char	*read_buffer;
+	static char	*read_buffer[OPEN_MAX];
 
 	read_bytes = 1;
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
+	if (fd < 0 || fd >= OPEN_MAX || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
+		return (NULL);
+	if (!read_buffer[fd])
+		read_buffer[fd] = (char *)malloc(1);
+	if (!read_buffer[fd])
 		return (NULL);
 	read_content = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!read_content)
 		return (NULL);
-	while (!(ft_strchr(read_buffer, '\n')) && read_bytes != 0)
+	while (!(ft_strchr(read_buffer[fd], '\n')) && read_bytes != 0)
 	{
 		read_bytes = read(fd, read_content, BUFFER_SIZE);
 		if (read_bytes == -1)
 		{
-			read_buffer = free_and_null(read_content, read_buffer);
+			read_buffer[fd] = free_and_null(read_content, read_buffer[fd]);
 			return (NULL);
 		}
 		*(read_content + read_bytes) = '\0';
-		read_buffer = ft_strjoin(read_buffer, read_content);
+		read_buffer[fd] = ft_strjoin(read_buffer[fd], read_content);
 	}
 	free(read_content);
-	read_content = read_line(read_buffer);
-	read_buffer = create_substring(read_buffer);
+	read_content = read_line(read_buffer[fd]);
+	read_buffer[fd] = create_substring(read_buffer[fd]);
 	return (read_content);
 }
-/*
+
 int	main(int argc, char **argv)
 {
 	int	fd;
 	char	*str;
 	
 	if (argc != 2)
+	{
 		printf ("Error introduce un parámetro\n");
+		return (1);
+	}
 	fd = open (argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		write(1, "Error", 5);
+		write(1, "Error\n", 6);
 		return (1);
 	}
 	str = get_next_line(fd);
-	printf ("%s \n", str);
+	printf ("%s", str);
 	close (fd);
+	write (1, "\n", 1);
 	return (0);
-}*/
-
+}
+/*
 int	main(void)
 {
 	int	fd;
+	int	fd2;
 	char	*str;
 
 	fd = open ("text.txt", O_RDONLY);
-	if (fd == -1)
+	fd2 = open ("text2.txt", O_RDONLY);
+	if (fd == -1 || fd2 == -1)
 	{
 		write (1,"Error", 5);
 		return (1);
 	}
 	str = get_next_line (fd);
-	printf ("1 %s\n", str);
+	if (str)
+	{
+		printf ("1 %s\n", str);
+		free (str);
+	}
+	str = get_next_line (fd2);
+	if (str)
+	{
+		printf ("2 %s\n", str);
+		free (str);
+	}
+	close (fd);
+	close (fd2);
 	return (0);
-}
+}*/
